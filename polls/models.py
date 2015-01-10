313dae63726346fa191cf761ba35f4d64a0e1da2
adiaviv05 <adiaviv05@gmail.com>
2015-01-10 21:00:02 +0200

    second commit

---
+++ polls/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.question
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def test_was_published_recently_with_old_poll(self):
+        """
+        was_published_recently() should return False for polls whose pub_date
+        is older than 1 day
+        """
+        old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=30))
+        self.assertEqual(old_poll.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_poll(self):
+        """
+        was_published_recently() should return True for polls whose pub_date
+        is within the last day
+        """
+        recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
+        self.assertEqual(recent_poll.was_published_recently(), True)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text
+
